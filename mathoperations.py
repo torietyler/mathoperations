72286f26c8e61dc47d907e8768f38ec6f532dd1f
torietyler <44810528+torietyler@users.noreply.github.com>
2018-11-19 08:34:12 -0700

    Add files via upload

---
+++ mathoperations.py
@@ -0,0 +1,31 @@
+
+'''
+Math with Python
+'''
+
+a = 25
+b = 10
+c = 7
+
+# Addition
+print("Display the sum of the numbers  ",a," +", b, " +", c, "=", a + b + c)
+
+print("The difference between  a, and b is :", a," -", b," =", a-b)
+
+print("Quotient a/b  :", a/b)
+
+#Integer division using //
+
+print("Integer division of 25 with 10 :", a//b)
+
+print(a//c)
+# The Remainder of an integer division %
+
+print(a%c)
+
+#Multiplication
+print("Product between 25 and 10 :", a*b)
+print(a, "*", c, "=", a*c)
+
+#Power base **exponent
+print(" 2 to the 5 power  :", 2**5)
